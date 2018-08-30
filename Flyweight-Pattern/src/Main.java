0c34b77d8e77050cfcbdf6870990f05321073732
ByeongGil-Jung <jbkcose@gmail.com>
2018-08-30 23:47:58 +0900

    [18-08-30] - Flyweight Pattern

---
+++ Flyweight-Pattern/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * Project: Design-Pattern
+ * ===========================================
+ * User: ByeongGil Jung
+ * Date: 2018-08-30
+ * Time: 오후 11:38
+ */
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello World ~");
+    }
+}
